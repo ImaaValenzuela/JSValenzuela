b0d2cd43517592af8674ccf567e4348177a2c0eb
enltd imaa <imanolvalenzuela03@gmail.com>
2022-06-13 22:54:14 -0300

    AlgoritmoUtilizandoCiclo

---
+++ js/miarchivo.js
@@ -0,0 +1,5 @@
+let ingresarUnNumero = parseInt(prompt("Ingrese un Numero"))
+for(let i = 1; i <= 10; i++){
+    let resultado = ingresarUnNumero * i;
+    alert(ingresarUnNumero + " x " + i + "=" + resultado)
+}
