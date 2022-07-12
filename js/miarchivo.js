0247638078b21ad3f5895527960a373d26559d38
imanol <imanolvalenzuela03@gmail.com>
2022-07-12 20:40:55 -0300

    Evntos

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,81 +1,78 @@
-class Producto{
-    constructor(nombre, precio, foto, categoria, id){
-        this.nombre = nombre
-        this.precio = precio
-        this.foto = foto
-        this.categoria = categoria
-        this.id = id
-    }
-};
-class Carrito {
-    constructor(id, productos) {
-    this.id = id;
-    this.productos = productos;
-    }
-    calcularTotal() {
-    let sumatoria = 0;
-    for(let i = 0; i < this.productos.length; i++) {
-        sumatoria = sumatoria + this.productos[i].precio; 
-    }
-    return sumatoria;
-    }
-};
-let carrito = new Carrito(1, []);
-let totalCompra = document.getElementById("totalCompra");
-let card = document.getElementById("card");
-const listaProductos = [];
-const catalogo = [];
-let nombre = prompt('¡Hola! Bienvenido a la Magic Tienda ¿Cómo es tu nombre?');
-const jack = new Producto ("Jack Black", 7100, "./media/jack.jpeg", "zapa", 1);
-const dakiti = new Producto ("Dakiti Black", 6500, "./media/dakiti.jpeg", "zapa", 2);
-const queen = new Producto ("Queen Black", 6200, "./media/queen.jpeg", "zapa", 3);
-const gorraLa = new Producto ("Gorra Los Angeles", 2000, "./media/gorrala.jpeg", "gorra", 4);
-const gorraNy = new Producto ("Gorra New York", 2000, "./media/gorrany.jpeg", "gorra", 5);
-const gorraRb = new Producto ("Gorra Red Bull", 2300, "./media/gorrarb.jpeg", "gorra", 6);
-catalogo.push(jack);
-catalogo.push(dakiti);
-catalogo.push(queen);
-catalogo.push(gorraLa);
-catalogo.push(gorraNy);
-catalogo.push(gorraRb);
+const productos = [
+    {id:01, tipo:"Zapas", nombre:"Dakiti Reflex", precio: 6500, foto: "./media/dakiti.jpeg" },
+    {id:02, tipo:"Zapas", nombre:"Queen Reflex", precio: 6300, foto: "./media/queen.jpeg" },
+    {id:03, tipo:"Zapas", nombre:"Jack Reflex", precio: 7000, foto: "./media/jack.jpeg" },
+    {id:04, tipo:"Gorras", nombre:"Gorra LA", precio: 2000, foto: "./media/gorrala.jpeg" },
+    {id:05, tipo:"Gorras", nombre:"Gorra NY", precio: 2000, foto: "./media/gorrany.jpeg" },
+    {id:06, tipo:"Gorras", nombre:"Gorra RB", precio: 2300, foto: "./media/gorrarb.jpeg" },
+];
 
-function cards(){
-for(const producto of listaProductos){
-    let columna = document.createElement("div");
-    columna.className = "col-md-3 col-sm-12 mt-5";
-    columna.id = `columna-${producto.id}`;
-    columna.innerHTML = 
-                `<div class="card">
-                    <img src=${producto.foto} style="width: 16rem; height: 16rem;" class="card-img-top" alt="Producto">
-                    <div class="card-body">
-                    <h5 class="card-title">${producto.nombre}</h5>
-                    <p class="card-text">$${producto.precio}</p>
-                    </div>
-                </div>`
-    card.append(columna);
+
+let carrito = [];
+let sectionProductos = document.getElementById("section-productos");
+let sectionCarrito = document.getElementById("section-carrito");
+
+let totalCompra = document.createElement("div");
+totalCompra.innerHTML = "<h2>Total: $</h2>";
+sectionCarrito.appendChild(totalCompra);
+
+let montoTotalCompra = document.createElement("h2");
+montoTotalCompra.innerText = "0";
+totalCompra.appendChild(montoTotalCompra);
+
+let cantidadProductos = document.createElement("div");
+cantidadProductos.innerHTML = "<h3>Mi Carrito: </h3>";
+sectionCarrito.appendChild(cantidadProductos);
+
+let cantProductos = document.createElement("h3");
+cantProductos.innerText = " 0";
+cantidadProductos.appendChild(cantProductos);
+
+let botonFinalizar = document.createElement("button");
+botonFinalizar.innerText = "Finalizar compra";
+sectionCarrito.appendChild(botonFinalizar);
+botonFinalizar.setAttribute("class", "boton");
+
+botonFinalizar.onclick = () => {
+    const precioFinal = montoTotalCompra.innerText;
+    alert("Muchas Gracias por su compra, el total es: $" + precioFinal);
+    vaciarCarrito();
 }
-};
 
-function seguirComprando(){
-let repregunta = prompt('¿Le gustaria comprar algo mas? (si/no)');
-let eleccion = repregunta.toUpperCase();
-if(eleccion == 'SI'){
-    menu();
-}else if(eleccion == 'NO'){
-    carrito.productos = listaProductos;
-    cards();
-    const total = carrito.calcularTotal();
-    totalCompra.innerHTML = `<p>El total de su compra es de: <b>$${total}</b></p>`;
-}else{
-    alert('Ingrese una opción valida');
-    seguirComprando();
+for (const producto of productos) {
+    let container = document.createElement("div");
+    container.setAttribute("class", "card-product");
+    container.innerHTML = ` <div class="img-container">
+                            <img src="${producto.foto}" alt="${producto.nombre}" class="img-product"/>
+                            </div>
+                            <div class="info-producto">
+                            <p class="font">${producto.nombre}</p>
+                            <strong class="font">$${producto.precio}</strong>
+                            <button class="boton" id="${producto.id}"> Agregar al carrito </button>
+                            </div>`;
+    sectionProductos.appendChild(container);
+    document.getElementById(`${producto.id}`).onclick = () => agregarAlCarrito(`${producto.id}`);
 }
-};
 
-function menu(){
-    let codigo = parseInt(prompt(`${nombre}, nuestro catalogo es el siguiente: \n 1. ${jack.nombre}: $${jack.precio} \n 2. ${dakiti.nombre}: $${dakiti.precio} \n 3. ${queen.nombre}: $${queen.precio} \n 4. ${gorraLa.nombre}: $${gorraLa.precio} \n 5. ${gorraNy.nombre}: $${gorraNy.precio} \n 6. ${gorraRb.nombre}: $${gorraRb.precio}\n IMPORTANTE: Por Favor seleccione un numero de la opcion que desee`));
-    let productoSeleccionado = catalogo.find(producto => producto.id == codigo);
-    listaProductos.push(productoSeleccionado);
-    seguirComprando();
-};
-menu();
+
+function agregarAlCarrito(id) {
+    carrito.push(productos.find(p => p.id == id));
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    calcularTotalCarrito();
+}
+
+function calcularTotalCarrito() {
+    let total = 0;
+    for (const producto of carrito) {
+        total += producto.precio;
+    }
+    montoTotalCompra.innerText = total;
+    cantProductos.innerText = carrito.length;
+}
+
+function vaciarCarrito() {
+    montoTotalCompra.innerText = "0";
+    cantProductos.innerText = "0";
+    localStorage.clear();
+    carrito=[];
+}
